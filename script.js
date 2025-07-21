8d82f8e47b5edf601c1459127c67c91101dce6cf
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-07-21 06:45:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+function openModal(){
+	document.getElementById("modal").style.visibility="visible";
+    document.body.style.background="lightgrey"
+}
+function closeModal(){
+	document.getElementById("modal").style.visibility="hidden";
+    document.body.style.background="white"
+}
